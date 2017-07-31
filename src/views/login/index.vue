c86c57e0fe98fe73fd730eb7c31451d786729ef8
LJY <dayanglee2015@163.com>
2017-07-31 11:57:20 +0800

    update login view

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,36 +1,54 @@
 <template>
   <div class="login-container">
+    <img class="logo" src='../../assets//images//login//logo.png'>
     <el-form autoComplete="on" :model="loginForm" :rules="loginRules" ref="loginForm" label-position="left" label-width="0px" class="card-box login-form">
-      <h3 class="title">汉王蓝天空感管理平台</h3>
+      <div class="title">
+        <span style="font-weight:bold;">LOGIN</span>
+        <span> SYSTEM</span>
+      </div>
       <el-form-item prop="acount" class="item">
-        <span class="svg-container">
-          <icon name="envelope" flip="horizontal"></icon>
-        </span>
-        <el-input name="acount" type="text" v-model="loginForm.acount" autoComplete="on" placeholder="邮箱/手机/用户名"></el-input>
+        <div class="icon-container">
+          <wscn-icon-svg icon-class="yonghu1" class="icon1"></wscn-icon-svg>
+        </div>
+        <input type="text" name="acount" autoComplete="on" placeholder="请输入用户名" v-model="loginForm.acount">
+        <!-- <el-input name="acount" type="text" v-model="loginForm.acount" autoComplete="on" placeholder="邮箱/手机/用户名"></el-input> -->
       </el-form-item>
       <el-form-item prop="password" class="item">
-        <span class="svg-container">
-          <icon name="lock"></icon>
-        </span>
-        <el-input name="password" type="password" @keyup.enter.native="handleLogin" v-model="loginForm.password" autoComplete="on" placeholder="密码"></el-input>
+        <div class="icon-container">
+          <wscn-icon-svg icon-class="mima" class="icon1"></wscn-icon-svg>
+        </div>
+        <input type="password" name="password" autoComplete="on" placeholder="请输入密码" v-model="loginForm.password">
       </el-form-item>
-      <el-form-item>
-        <el-button type="primary" style="width:40%;float:left;" :loading="loading" @click.native.prevent="handleLogin">
+      <el-form-item class="item item1">
+        <el-button class="login-button" :loading="loading" @click.native.prevent="handleLogin">
           登录
         </el-button>
-        <el-button type="primary" style="width:40%;float:right;">
-          忘记密码?
-        </el-button>
       </el-form-item>
-  
-      <div class='tips'>admin账号为: admin@hanwang.com.cn</div>
-      <div class='tips'>agent账号: agent@hanwang.com.cn</div>
-      <div class='tips'>user账号: user@hanwang.com.cn</div>
-      <div class='tips'>密码随便填</div>
-      <!-- <router-link to="/sendpwd" class="forget-pwd">
-                        忘记密码?(或首次登录)
-                      </router-link> -->
     </el-form>
+    <div class="dcode-container">
+      <div class="dcode">
+        <div>
+          <img src="../../assets//images//login//weixin_erweima.png" class="img1">
+        </div>
+        <div>
+          <img src="../../assets//images//login//weixinlogo.png" class="img2">
+          <p>汉王蓝天</p>
+        </div>
+  
+      </div>
+      <div class="dcode">
+        <div>
+          <img src="../../assets//images//login//weibo_erweima.png" class="img1">
+        </div>
+        <div>
+          <img src="../../assets//images//login//weibologo.png" class="img2">
+          <p>汉王蓝天</p>
+        </div>
+      </div>
+    </div>
+    <div class="copyright">
+      Copyright &nbsp;&copy;&nbsp; 汉王蓝天科技有限公司
+    </div>
   </div>
 </template>
 
@@ -72,13 +90,13 @@ export default {
     }
   },
   methods: {
-    handleLogin () {
+    handleLogin() {
       this.$refs.loginForm.validate(valid => {
-        if ( valid ) {
+        if (valid) {
           this.loading = true;
           this.$store.dispatch('LoginByAcount', this.loginForm).then(() => {
             this.loading = false;
-            this.$router.push( { path: '/dashboard/index' } );
+            this.$router.push({ path: '/dashboard/index' });
           }).catch(err => {
             this.$message.error(err);
             this.loading = false;
@@ -93,75 +111,161 @@ export default {
 }
 </script>
 
-<style lang="scss" rel="stylesheet/scss">
-@import '../../assets/style/mixin.scss';
-
+<style lang="scss" rel="stylesheet/scss" scoped>
+// @import '../../assets/style/mixin.scss';
 .login-container {
-  @include relative;
+  // @include relative;
   width: 100%;
   height: 100vh;
-  background-color: #2d3a4b;
+  background-image: url('../../assets/images/login/bj.jpg');
+  background-repeat: no-repeat;
+  background-size: cover;
 
+  .logo {
+    width: 300px;
+    height: 80px;
+    margin-top: 50px;
+    margin-left: 50px;
+  }
   .login-form {
     position: absolute;
-    left: 0;
-    right: 0;
+    top: 50%;
+    margin-top: -190px;
+    left: 50%;
+    margin-left: -250px;
     width: 500px;
+    height: 380px;
     padding: 35px 35px 15px 35px;
-    margin: 120px auto;
-    background-color: white;
-    -moz-box-shadow: 2px 2px 15px rgba(135, 206, 235, 1);
-    -webkit-box-shadow: 2px 2px 15px rgba(135, 206, 235, 1);
-    box-shadow: 2px 2px 15px rgba(135, 206, 235, 1);
+    background-color: rgba(12, 26, 44, 0.12);
+    border-radius: 8px;
   }
 
   .title {
-    font-size: 26px;
-    font-weight: 400;
-    color: black;
+    font-family: Helvetica;
+    font-size: 48px;
+    color: #fff;
     margin: 0px auto 40px auto;
     text-align: center;
+    font-weight: 200;
   }
 
   .item {
-    border: 1px solid rgba(255, 255, 255, 0.1);
-    background: rgba(0, 0, 0, 0.1);
-    border-radius: 5px;
-    color: #454545;
+    width: 280px;
+    height: 50px;
+    line-height: 50px;
+    margin: 20px auto;
+    color: white;
+    vertical-align: middle;
+    border-color: white;
+    border-style: solid;
+    border-top-width: 0px;
+    border-right-width: 0px;
+    border-bottom-width: 1px;
+    border-left-width: 0px;
+
+    .icon-container {
+      width: 45px;
+      height: 50px;
+      float: left;
+    }
+    .icon1 {
+      width: 30px;
+      height: 50px;
+    }
+
+    input {
+      width: 230px;
+      height: 50px;
+      background-color: rgba(12, 26, 44, 0);
+      border-color: white;
+      border-style: solid;
+      border-top-width: 0px;
+      border-right-width: 0px;
+      border-bottom-width: 0px;
+      border-left-width: 0px;
+      outline: none;
+      font-size: 18px;
+      color: white;
+    }
   }
 
-  input:-webkit-autofill {
-    // -webkit-box-shadow: 0 0 0px 1000px #293444 inset !important;
-    -webkit-text-fill-color: #fff !important;
+  .item1 {
+    border-bottom-width: 0;
   }
-  input {
-    background: transparent;
-    border: 0px;
-    -webkit-appearance: none;
-    border-radius: 0px;
-    padding: 12px 5px 12px 15px;
-    color: black;
-    height: 47px;
-    font-size: 20px;
-  }
-  .el-input {
-    display: inline-block;
-    height: 47px;
-    width: 85%;
-  }
-  .svg-container {
-    padding: 6px 5px 6px 15px;
-    color: #889aa4;
-  }
-
-  .forget-pwd {
-    color: #fff;
+  .login-button {
+    width: 100%;
+    height: 45px;
+    margin-top: 20px;
+    background-color: rgb(40, 78, 123);
+    outline: none;
+    color: white;
+    border: 0;
+    font-size: 18px;
   }
 }
 
-.tips {
-  font-size: 14px;
-  color: rgba(135, 206, 235, 1);
-  margin-bottom: 5px;
+.tip-container {
+  position: absolute;
+  left: 50%;
+  margin-left: -250px;
+  width: 500px;
+  padding: 35px 35px 15px 35px;
+  .tips {
+    font-size: 14px;
+    color: rgba(135, 206, 235, 1);
+    margin-bottom: 5px;
+  }
+}
+
+.dcode-container {
+  position: absolute;
+  right: 30px;
+  bottom: 80px;
+  .dcode {
+    float: left;
+    width: 100px;
+    margin-right: 10px;
+    .img1 {
+      width: 100px;
+    }
+    .img2 {
+      width: 30px;
+    }
+    p {
+      color: #fff;
+      font-size: 14px;
+      margin-top: 0px;
+      margin-right: 8px;
+      float: right;
+    }
+  }
+}
+
+.copyright {
+  font-size: 20px;
+  color: white;
+  position: absolute;
+  left: 0;
+  right: 0;
+  bottom: 30px;
+  width: 330px;
+  margin: 0 auto;
+}
+
+
+input::-webkit-input-placeholder {
+  color: rgba(255, 255, 255, .6);
+}
+
+input:-moz-placeholder {
+  color: rgba(255, 255, 255, .6);
+}
+
+input::-moz-placeholder {
+  color: rgba(255, 255, 255, .6);
+}
+
+input:-ms-input-placeholder {
+  color: rgba(255, 255, 255, .6);
 }
 </style>
